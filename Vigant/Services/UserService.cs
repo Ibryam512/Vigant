534cdb0be19aa18ab75d35f970a3959b5d7ff9e7
Ibryam <57998847+Ibryam512@users.noreply.github.com>
2021-10-16 15:43:16 +0300

    Preventing the null cases in UserService

--- Vigant/Services/UserService.cs
+++ Vigant/Services/UserService.cs
@@ -23,8 +23,8 @@ namespace Vigant.Services
 
         public Task<List<ApplicationUser>> GetUsers() => this._context.Users.ToListAsync();
 
-        public List<ApplicationUser> ShowFriends(string userId) => GetUsers().Result.Single(x => x.Id == userId).Friends;
+        public List<ApplicationUser> ShowFriends(string userId) => GetUsers().Result.Single(x => x.Id == userId).Friends ?? new List<ApplicationUser>();
 
-        public List<ApplicationUser> ShowParticipants(string interestId) => this._context.Interests.Single(x => x.Id == interestId).Participants;
+        public List<ApplicationUser> ShowParticipants(string interestId) => this._context.Interests.Single(x => x.Id == interestId).Participants ?? new List<ApplicationUser>();
     }
 }
